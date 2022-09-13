9c3fd16223b847d8002a8455efb2cb6dfdba2109
Rasif <rasiftagizade11@gmail.com>
2022-09-13 17:53:46 +0400

    Movies App with React Js

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <div className="inner-content">
+          <div className="brand">Izlənəcəklər</div>
+
+          <ul className="nav-links">
+            <li>Izlenenler</li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
